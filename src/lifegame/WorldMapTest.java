16a236df3f5591b21c86494ca8048bf16ddf4284
zasm21 <2773188782@qq.com>
2019-06-17 15:45:13 +0800

    lifegame_java

---
+++ src/lifegame/WorldMapTest.java
@@ -0,0 +1,34 @@
+package lifegame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class WorldMapTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void testPaintComponentGraphics() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testJPanelBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetUIPanelUI() {
+		fail("Not yet implemented");
+	}
+
+}
